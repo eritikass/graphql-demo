42bc18ca29f6f6a68886aca1b4b7e102ab633003
Siim Tiilen <eritikass@gmail.com>
2020-01-25 23:39:16 +0200

    add express-graphql demo

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
 # GraphQL demo
 
 This project contains [Express](https://expressjs.com/) hello world app.
+
+## Install GraphQL tooling
+
+```bash
+npm i graphql express-graphql
+```
+
+## Define schema
+
+```graphql
+  type Message {
+    ID: ID!
+    title: String!
+    body: String
+  }
+
+  type Query {
+    messages: [Message]
+  }
+  type Mutation {
+    addMessage(title: String!, body: String): Message
+  }
+```
+
+## Add GraphQL serve code
+
+```js
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const messages = [];
+const root = {
+  messages: () => {
+    return [...messages].reverse();
+  },
+  addMessage: (message) => {
+    message.ID = messages.length;
+    messages.push(message);
+    return message;
+  }
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(schema),
+    rootValue: root,
+    graphiql: true,
+  }),
+);
+```
+
+## Add optional limit parameter to messages query
+
+
+```graphql
+  type Query {
+    messages(limit: Int = 3): [Message]
+  }
+```
+
+```
+  messages: (params) => {
+    return [...messages].reverse().slice(0, params.limit);
+  },
+```
--- index.js
+++ index.js
@@ -1,8 +1,57 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// Construct a schema, using GraphQL schema language
+const graphqlSchema = `
+  type Message {
+    ID: ID!
+    title: String!
+    body(trim: Int): String
+  }
+
+  type Query {
+    messages(limit: Int = 3): [Message]
+  }
+  type Mutation {
+    addMessage(title: String!, body: String): Message
+  }
+`;
+
+const fieldResolver = {
+  Message: {
+    body: (a, b, c) => {
+      console.log('body', a, b, c);
+      return null;
+    },
+  }
+}
+
+const messages = [];
+const root = {
+  messages: params => {
+    return [...messages].reverse().slice(0, params.limit);
+  },
+  addMessage: message => {
+    message.ID = messages.length;
+    messages.push(message);
+    return message;
+  }
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(graphqlSchema),
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/`);
